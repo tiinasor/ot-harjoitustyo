4dafa54c746ac80f049ccde2bd1dc349155241e3
Sormunen Tiina M <tiinasor@cubbli22-gold.local>
2025-04-01 21:27:02 +0300

    Add user creation test

---
+++ src/tests/user_service_test.py
@@ -0,0 +1,12 @@
+import unittest
+from services.user_service import user_service
+from repositories.user_repository import user_repository
+from models.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def test_creating_new_user_works(self):
+        user_service.create_user('user1', 'password1')
+
+        found_user = user_repository.find_user('user1')
+        self.assertIsNotNone(found_user)
+        self.assertEqual(found_user.username, 'user1')
