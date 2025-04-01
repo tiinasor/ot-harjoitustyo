8a6687702c4a6628d678f7cf9f8b5648f9b1d0d3
Sormunen Tiina M <tiinasor@cubbli22-gold.local>
2025-04-01 22:06:43 +0300

    Update class name

--- src/tests/user_service_test.py
+++ src/tests/user_service_test.py
@@ -3,7 +3,7 @@ from services.user_service import user_service
 from repositories.user_repository import user_repository
 from models.user import User
 
-class TestUserRepository(unittest.TestCase):
+class TestUserService(unittest.TestCase):
     def test_creating_new_user_works(self):
         user_service.create_user('user1', 'password1')
 
